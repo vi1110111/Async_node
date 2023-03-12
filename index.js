f27536f24af518dfe46709f020509a6f44d4e152
Your Name <you@example.com>
2023-03-12 18:33:36 +0100

    Promise structure

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body);
-
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
-        console.log("Dog img writed");
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const whiteFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Could not white the file");
+      resolve("Success");
     });
-});
+  });
+};
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file!");
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body);
+
+    return whiteFilePro("dog-img.txt", res.body.message);
+  })
+  .then(() => {
+    console.log("Random img written!");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 // some changes
