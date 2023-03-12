efd5b575e8a8aafb4a9d431985109bd4a1cdc7a0
Your Name <you@example.com>
2023-03-12 19:10:28 +0100

    Async/await

--- index.js
+++ index.js
@@ -19,21 +19,74 @@ const readFilePro = (file) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+// Async / await
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body);
 
-    return whiteFilePro("dog-img.txt", res.body.message);
-  })
-  .then(() => {
-    console.log("Random img written!");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    const res1Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
 
-// some changes
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const img = all.map((elem) => {
+      return elem.body.message;
+    });
+
+    console.log(img);
+
+    await whiteFilePro("dog-img.txt", img.join("\n"));
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return "2: READY!";
+};
+
+(async () => {
+  try {
+    console.log("1: Start");
+    const x = await getDogPic();
+    console.log(x);
+    console.log("3: Finish");
+  } catch (error) {
+    console.log("EERRRROOORRR");
+  }
+})();
+
+// console.log("1: Start");
+// getDogPic()
+//   .then((x) => {
+//     console.log(x);
+//     console.log("3: Finish");
+//   })
+//   .catch((err) => {
+//     console.log("EERRRROOORRR");
+//   });
+
+// Promise
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body);
+
+//     return whiteFilePro("dog-img.txt", res.body.message);
+//   })
+//   .then(() => {
+//     console.log("Random img written!");
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+
+// some changes!
